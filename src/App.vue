cd013f2bf3f4b6ddcd9c70996648c3a64ab83822
shafi <frshafi49@gmail.com>
2020-01-15 03:39:28 +0600

    show data,install vuex

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="app">
-
+    <product-list-one v-bind:products="products" />
+    <product-list-two v-bind:products="products" />
   </div>
 </template>
 
 <script>
-
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-
+    ProductListOne,
+    ProductListTwo
+  },
+  data() {
+    return {
+      products:[
+        {name:'Banana Skin',price:20},
+        {name:'Shiny Star',price:40},
+        {name:'Green Shells',price:60},
+        {name:'Red Shells',price:80},
+      ]
+    };
   }
-}
+};
 </script>
 
-<style scoped>
+<style>
+body{
+  font-family: Ubuntu;
+  color:#555
 
-
+}
 </style>
